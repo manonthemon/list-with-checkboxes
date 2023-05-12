28f94ebcde60ac7219739419144a062159171567
Manonthemon <klimowicz.maciej@gmail.com>
2023-05-12 13:24:27 +0100

    Added code to check boxes when shift pressed

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-
 //selects all checkboxes
 const checkboxes = document.querySelectorAll('.inbox input[type = "checkbox"]');
 
+// Variable for the last checked box
+let lastChecked;
+
 //functions handling click event
 function handleCheck(e) {
-    console.log(e)
+  //Check if the shift key is down AND if the checkbox is being checked
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach((checkbox) => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+
+  lastChecked = this; //Marks the checked box as last checked
 }
 
 //adding event listeners to all checkboxes
-checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck))
+checkboxes.forEach((checkbox) =>
+  checkbox.addEventListener("click", handleCheck)
+);
